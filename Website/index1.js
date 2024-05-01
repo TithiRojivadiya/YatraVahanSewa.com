a3af261c76295bf972362dd25adb196a0f58afb8
mukti2216patel <mukti2216patel@gmail.com>
2024-05-02 00:41:21 +0530

    null

---
+++ Website/index1.js
@@ -0,0 +1,20 @@
+
+document.addEventListener('DOMContentLoaded', function(){
+    var rideButtons = document.querySelectorAll('.instantride .btn');
+    for (var i = 0; i < rideButtons.length; i++) {
+        rideButtons[i].addEventListener('click', function(event) {
+            event.preventDefault();
+            var box = this.closest('.box');
+            var key = box.querySelector('h3').textContent.trim();
+            var valueString = box.querySelector('h2').textContent.trim();
+        
+                var value = parseFloat(valueString.replace('₹', '').replace(',', ''));
+                
+            console.log(value);
+            
+
+            var url = 'form_ir.html?key=' + encodeURIComponent(key) + '&value=' + encodeURIComponent(value);
+            window.location.href = url;
+        });
+    }
+});
