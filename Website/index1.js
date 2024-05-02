8c4e97a24563972037e977e1b4568202e8e17824
TithiRojivadiya <trojivadiya@gmail.com>
2024-05-02 22:50:35 +0530

    changes

--- Website/index1.js
+++ Website/index1.js
@@ -18,3 +18,25 @@ document.addEventListener('DOMContentLoaded', function(){
         });
     }
 });
+
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    var rideButtons = document.querySelectorAll('.rentacar .btn');
+    for (var i = 0; i < rideButtons.length; i++) {
+        rideButtons[i].addEventListener('click', function(event) {
+            event.preventDefault();
+            var box = this.closest('.box');
+            var key = box.querySelector('h3').textContent.trim();
+            var valueString = box.querySelector('h2').textContent.trim();
+        
+                var value = parseFloat(valueString.replace('₹', '').replace(',', ''));
+                
+            console.log(value);
+            
+
+            var url = 'form_rc.html?key=' + encodeURIComponent(key) + '&value=' + encodeURIComponent(value);
+            window.location.href = url;
+        });
+    }
+});
